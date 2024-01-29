264d6a5e2191a2ac29215ed651b79c316e2ca9f2
India <India@India-PC>
2024-01-29 23:05:11 +0530

    m

---
+++ main.tf
@@ -0,0 +1,85 @@
+	resource "aws_security_group" "security_group" {
+	  name        = "${var.env}-${var.alb_type}-sg"
+	  description = "${var.env}-${var.alb_type}-sg"
+	  vpc_id      = var.vpc_id
+	
+	  ingress {
+	    description = "HTTP"
+	    from_port   = 80
+	    to_port     = 80
+	    protocol    = "tcp"
+	    cidr_blocks = [var.alb_sg_allow_cidr]
+	  }
+	  
+	  ingress {
+	    description = "HTTPS"
+	    from_port   = 443
+	    to_port     = 443
+	    protocol    = "tcp"
+	    cidr_blocks = [var.alb_sg_allow_cidr]
+	  }
+	
+	  egress {
+	    from_port   = 0
+	    to_port     = 0
+	    protocol    = "-1"
+	    cidr_blocks = ["0.0.0.0/0"]
+	  }
+	
+	  tags = {
+	    Name = "${var.env}-${var.alb_type}-sg"
+	  }
+	}
+	
+	resource "aws_lb" "alb" {
+	  name               = "${var.env}-${var.alb_type}"
+	  internal           = var.internal
+	  load_balancer_type = "application"
+	  security_groups    = [aws_security_group.security_group.id]
+	  subnets            = var.subnets
+	  tags = {
+	    Environment = "${var.env}-${var.alb_type}"
+	  }
+	}
+	
+	resource "aws_route53_record" "www" {
+	  zone_id = var.zone_id
+	  name    = var.dns_name
+	  type    = "CNAME"
+	  ttl     = 300
+	  records = [aws_lb.alb.dns_name]
+	}
+	
+	resource "aws_lb_listener" "listener-http-public" {
+	  count             = var.alb_type == "public" ? 1 : 0
+	  load_balancer_arn = aws_lb.alb.arn
+	  port              = "80"
+	  protocol          = "HTTP"
+	
+	 default_action {
+     type             = "redirect"
+         redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    	}
+  	  }
+	}
+	
+	resource "aws_lb_listener" "listener" {
+	  load_balancer_arn = aws_lb.alb.arn
+	  port              = var.port
+      protocol          = var.protocol
+      ssl_policy        = var.ssl_policy
+      certificate_arn   = var.certificate_arn
+	
+	
+	  default_action {
+	    type             = "fixed-response"
+	    fixed_response {
+        content_type = "text/plain"
+        message_body = "Invalid Page"
+        status_code  = "404"
+    }
+	  }
+	}
---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "alb_dns_name" {
+  value = aws_lb.alb.dns_name
+}
+
+output "alb_arn" {
+  value = aws_lb.alb.arn
+}
+
+output "listener_arn" {
+  value = aws_lb_listener.listener.arn
+}
